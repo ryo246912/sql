897a8a0969fdfe5313fec77565222066b180c1a1
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2022-08-02 22:32:17 +0900

    [add]: CASE式 2
    ・CASE＋集合関数 GROUP BY col
    SELECT  集合関数(CASE WHEN ~ THEN END) as col
    FROM tal
    GROUP BY  col

--- 1.sql
+++ 1.sql
@@ -1,3 +1,52 @@
+-- 異なる条件の集計を1SQLで行う
+-- MEMO CASE式の最後のELSE ~ ENDを忘れずに
+-- MEMO 集計する際は、集合関数 GROUP BY col
+SELECT  CASE WHEN pref_name = '徳島' THEN '四国'
+             WHEN pref_name = '香川' THEN '四国'
+             WHEN pref_name = '愛媛' THEN '四国'
+             WHEN pref_name = '高知' THEN '四国'
+             WHEN pref_name = '福岡' THEN '九州'
+             WHEN pref_name = '佐賀' THEN '九州'
+             WHEN pref_name = '長崎' THEN '九州'
+             ELSE 'その他' END AS district
+       ,SUM(population)
+FROM "PopTbl"
+GROUP BY  district;
+
+-- 上記のクエリを冗長に書くと下になる
+SELECT  CASE WHEN pref_name = '徳島' THEN '四国'
+             WHEN pref_name = '香川' THEN '四国'
+             WHEN pref_name = '愛媛' THEN '四国'
+             WHEN pref_name = '高知' THEN '四国'
+             WHEN pref_name = '福岡' THEN '九州'
+             WHEN pref_name = '佐賀' THEN '九州'
+             WHEN pref_name = '長崎' THEN '九州'
+             ELSE 'その他' END
+       ,SUM(population)
+FROM "PopTbl"
+GROUP BY  CASE WHEN pref_name = '徳島' THEN '四国'
+             WHEN pref_name = '香川' THEN '四国'
+             WHEN pref_name = '愛媛' THEN '四国'
+             WHEN pref_name = '高知' THEN '四国'
+             WHEN pref_name = '福岡' THEN '九州'
+             WHEN pref_name = '佐賀' THEN '九州'
+             WHEN pref_name = '長崎' THEN '九州'
+             ELSE 'その他' END;
+
+SELECT  CASE WHEN population < 100 THEN '01'
+             WHEN population >= 100 AND population < 200 THEN '02'
+             WHEN population >= 200 AND population < 300 THEN '03'
+             WHEN population >= 300 THEN '04'  
+             ELSE NULL END AS pop_class
+       ,COUNT(*)                                             AS cnt
+FROM "PopTbl"
+GROUP BY pop_class;
+
+SELECT  pref_name
+       ,SUM( CASE WHEN sex = '1' THEN population ELSE 0 END) AS cnt_m
+       ,SUM( CASE WHEN sex = '2' THEN population ELSE 0 END) AS cnt_f
+FROM "PopTbl2"
+GROUP BY  pref_name;
 -- MEMO サブクエリ内のテーブル呼出は、クエリ内で呼出していたらそのまま使える(FROM "CourseMaster" AS CM)
 SELECT CM.course_name,
        CASE WHEN (SELECT COUNT(*) FROM "OpenCourse" AS OC WHERE CM.course_id = OC.course_id AND OC.month LIKE '%06' ) > 0 THEN '○' ELSE '×' END as "6月",
